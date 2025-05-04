4496bd6e5e666318db0f3d95f51f2d5094c001ff
OshkarVTec <a01275287@tec.mx>
2025-05-03 21:40:23 -0600

    add openmp

--- reto.c
+++ reto.c
@@ -8,10 +8,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <omp.h>
 #include "reto_librerias.h"
 
-int main(){
-    grey_scale_img("greyscale_1","./img/sample1.bmp");
+NUM_THREADS = 4; // Number of threads
 
+int main(){
+    omp_set_num_threads(NUM_THREADS);
+    grey_scale_img("greyscale_1","./img/sample1.bmp");
+    #pragma omp parallel
+    {
+        #pragma omp single
+        {
+            printf("Number of threads used: %d\n", omp_get_num_threads());
+        }
+    }
     return 0;
 }
