049b1254cb554c69cf52245c009a84155d366311
CruzPer823 <a01736214@tec.mx>
2025-05-06 01:28:20 -0600

    Feat: Add colored mirroring

--- reto.c
+++ reto.c
@@ -5,7 +5,7 @@
 // David Alberto Alvarado Cabrero
 // Mayo 2025
 
-// IMPORTANT run: gcc reto.c -o reto -lm.
+// IMPORTANT run: gcc reto.c -o reto -lm -fopenmp
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,6 +21,8 @@ int main()
     omp_set_num_threads(NUM_THREADS);
     grey_scale_img("greyscale_1", "./img/sample1.bmp");
     blur_img("blur_1", "./img/sample1.bmp", 55);
+    horizontal_mirror_color_img("mirrorHorizontal_1", "./img/sample1.bmp");
+    vertical_mirror_color_img("mirrorVertical_2", "./img/sample1.bmp");
 #pragma omp parallel
     {
 #pragma omp single
