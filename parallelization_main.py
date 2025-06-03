4045b333cc3be687f97e411f8e142d23abf1c043
OshkarVTec <a01275287@tec.mx>
2025-06-03 17:24:14 -0600

    fix: add missing import

--- parallelization_main.py
+++ parallelization_main.py
@@ -7,6 +7,7 @@ from PyQt6.QtCore import QThread, pyqtSignal
 from parallelization_ui import (
     Ui_MainWindow,
 )
+import time
 
 COMMAND = "mpiexec -np 3 --hostfile mirror/Parallelization/machinefile mirror/Parallelization/reto"
 OUTPUT_FOLDER = "output"
